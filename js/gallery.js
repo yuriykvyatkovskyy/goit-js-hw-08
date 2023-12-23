42d8309f41cb2a8d9ab6e6024ad6f2b8d757b8c9
yuriykvyatkovskyy <yuriykreik20021997@gmail.com>
2023-12-23 16:50:05 +0200

    create <li> and create css

--- js/gallery.js
+++ js/gallery.js
@@ -65,4 +65,24 @@ const images = [
     description: "Lighthouse Coast Sea",
   },
 ];
- 
+
+const gallery = document.querySelector('.gallery');
+
+const galleryItem = ({ preview, original, description }) => {
+  return `
+  <li class="gallery-item">
+  <a class="gallery-link" href="${original}">
+    <img
+      class="gallery-image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`;
+};
+
+const galleryMarkup = images.map(galleryItem).join('');
+
+gallery.innerHTML = galleryMarkup;
